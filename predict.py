9adc67c2aca65993f54fba7801096efbafc7a704
root <root@instance-2.us-central1-a.c.stickwarriors2.internal>
2021-07-21 12:12:10 +0000

    Change order col format date

--- predict.py
+++ predict.py
@@ -3,7 +3,7 @@ import preprocess as pp
 import pandas as pd
 from datetime import date
 from datetime import timedelta
-path = '/home/suv/ML/'
+path = '/root/git_ml/'
 data_path = path + 'input/ML_{}.csv'.format((date.today() - timedelta(1)).strftime("%Y-%m-%d"))
 country = path + 'country.txt'
 feature = path + 'feature_col.txt'
@@ -16,5 +16,5 @@ data = rf.getData(data_path)
 X, id, act_date = pp.preprocess(data, country, feature, ndays)
 
 my_model = rf.getModel(model)
-output = pd.DataFrame({'act_date':act_date,'resettable_device_id_or_app_instance_id':id, 'predicted_45':my_model.predict(X)})
+output = pd.DataFrame({'resettable_device_id_or_app_instance_id':id, 'predicted_45':my_model.predict(X),'act_date':act_date })
 output.to_csv(output_path, index = False)
